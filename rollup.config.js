00ff60576391e84ea5db2b603689940081596d58
Valmisson Grizorte <missongrizorte@gmail.com>
2019-09-25 14:44:50 -0300

    instaled postcss

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,11 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import livereload from 'rollup-plugin-livereload'
 import { terser } from 'rollup-plugin-terser'
+import postcss from 'rollup-plugin-postcss'
+
+// postcss plugins
+import postcssImport from 'postcss-import'
+import postcssPresetEnv from 'postcss-preset-env'
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -18,11 +23,26 @@ export default {
     svelte({
       // enable run-time checks when not in production
       dev: !production,
+
+      emitCss: true,
+
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
         css.write('public/bundle.css')
       }
+    }),
+
+    postcss({
+      plugins: [
+        postcssImport(),
+        postcssPresetEnv({
+          stage: 0,
+          browsers: 'last 2 versions'
+        })
+      ],
+      extract: true,
+      extensions: ['.css']
     }),
 
     // If you have external dependencies installed from
