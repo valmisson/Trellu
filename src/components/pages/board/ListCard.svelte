6cfd5bc4725afeb91ea603efa1add2cf4ad217af
Valmisson Grizorte <missongrizorte@gmail.com>
2019-10-01 15:24:02 -0300

    creating form update card

--- src/components/pages/board/ListCard.svelte
+++ src/components/pages/board/ListCard.svelte
@@ -1,5 +1,13 @@
 <script>
   export let title
+
+  let showFormEditCard = false
+
+  const focusInput = el => el.focus()
+
+  const toggleFormEditCard = () => {
+    showFormEditCard = !showFormEditCard
+  }
 </script>
 
 <style>
@@ -13,6 +21,71 @@
   .list-card:last-child {
     margin-bottom: 0;
   }
+
+  .list-card:hover .btn-edit-card {
+    opacity: 1;
+    visibility: visible;
+    transition: all ease 500ms;
+  }
+
+  /* card info */
+
+  .card-info {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .btn-edit-card {
+    color: var(--text-light);
+    font-size: 13px;
+    margin-top: -4px;
+    opacity: 0;
+    visibility: hidden;
+    transition: all ease 500ms;
+  }
+
+  .btn-edit-card:hover {
+    color: var(--text-dark);
+  }
+
+  /* form edit card */
+  .form-edit-card .btn-close {
+    float: right;
+    font-size: 16px;
+    margin-bottom: 15px;
+  }
+
+  .form-edit-card > div {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+  }
+
+  .btn-delete-card {
+    color: var(--text-red);
+    font-size: 14px;
+    font-weight: 500;
+  }
 </style>
 
-<li class="list-card">{ title }</li>
+<li class="list-card">
+  {#if !showFormEditCard}
+    <div class="card-info">
+      <span>{ title }</span>
+      <button class="btn-edit-card icon-edit" on:click={toggleFormEditCard}></button>
+    </div>
+  {/if}
+
+  {#if showFormEditCard}
+    <div class="form-edit-card">
+      <button class="btn-close icon-close" on:click={toggleFormEditCard}></button>
+
+      <textarea placeholder="Digite o nome do cartão" rows="2" bind:value={title} use:focusInput></textarea>
+
+      <div>
+        <button class="btn-delete-card">EXCLUIR</button>
+        <button class="btn btn-primary">ATUALIZAR</button>
+      </div>
+    </div>
+  {/if}
+</li>
