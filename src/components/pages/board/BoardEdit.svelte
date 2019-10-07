238544e633b8b0e0b8ace1680fa232873e1dbc19
Valmisson Grizorte <missongrizorte@gmail.com>
2019-10-06 22:05:11 -0300

    component split board editing

---
+++ src/components/pages/board/BoardEdit.svelte
@@ -0,0 +1,146 @@
+<script>
+  import { fly } from 'svelte/transition'
+  import { router } from '@spaceavocado/svelte-router'
+  import generateLink from '@utils/generateLink.js'
+  import BoardsDB from '@datastore/Boards.js'
+
+  export let id
+  export let name
+  export let toggleEdit
+
+  const inputFocus = el => el.focus()
+
+  async function updateBoard () {
+    if (!name) return
+
+    await BoardsDB.update(id, name)
+
+    toggleEdit()
+
+    // update url
+    $router.push(generateLink(name, id))
+    location.reload()
+  }
+
+  async function deleteBoard () {
+    await BoardsDB.remove(id)
+
+    $router.push('/')
+    location.reload()
+  }
+</script>
+
+<style>
+  /* board edit */
+
+  .board-edit {
+    background-color: var(--white);
+    box-shadow: -3px 0 5px rgba(0, 0, 0, 0.05);
+    height: calc(100vh - 50px);
+    padding: 20px;
+    position: fixed;
+    right: 0;
+    top: 50px;
+    width: 290px;
+    z-index: 997;
+  }
+
+  /* .board edit header */
+
+  .board-edit-header {
+    align-items: center;
+    display: flex;
+    color: var(--text-dark);
+    justify-content: space-between;
+  }
+
+  .board-edit-header > span {
+    font-weight: 500;
+  }
+
+  .board-edit-header > button {
+    color: var(--text-light);
+    padding: 5px;
+  }
+
+  /* board edit form */
+
+  .board-edit-form {
+    display: table;
+    margin-top: 40px;
+  }
+
+  .board-edit-input {
+    margin-top: 10px;
+  }
+
+  .board-btn-update {
+    float: right;
+    margin-top: 20px;
+  }
+
+  /* board edit delete */
+
+  .board-edit-delete {
+    margin-top: 100px;
+    width: 100%;
+  }
+
+  .board-btn-delete {
+    color: var(--text-red);
+    font-size: 14px;
+    font-weight: 500;
+    float: right;
+    margin-right: -5px;
+    padding: 5px;
+  }
+
+  @media (min-width: 768px) {
+    /* board edit */
+
+    .board-edit {
+      width: 340px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    /* board edit */
+
+    .board-edit {
+      height: calc(100vh - 60px);
+      top: 60px;
+    }
+
+    /* board edit delete */
+
+    .board-edit-delete {
+      position: absolute;
+      bottom: 50px;
+    }
+
+    .board-btn-delete {
+      margin-right: 40px;
+    }
+  }
+</style>
+
+<aside class="board-edit" transition:fly={{ x: 150 }}>
+  <header class="board-edit-header">
+    <span>Editar Quadro</span>
+
+    <button class="icon-close" on:click={toggleEdit}></button>
+  </header>
+
+  <div class="board-edit-form">
+    <label>Nome</label>
+
+    <textarea type="text" class="board-edit-input" placeholder="Digite o nome do quadro" rows="3"
+      bind:value={name} use:inputFocus></textarea>
+
+    <button class="board-btn-update btn btn-primary" on:click={updateBoard} disabled={!name}>ATUALIZAR</button>
+  </div>
+
+  <div class="board-edit-delete">
+    <button class="board-btn-delete" on:click={deleteBoard}>EXCLUIR QUADRO</button>
+  </div>
+</aside>
--- src/components/pages/board/Header.svelte
+++ src/components/pages/board/Header.svelte
@@ -1,8 +1,5 @@
 <script>
-  import { fly } from 'svelte/transition'
-  import { router } from '@spaceavocado/svelte-router'
-  import generateLink from '@utils/generateLink.js'
-  import BoardsDB from '@datastore/Boards.js'
+  import BoardEdit from './BoardEdit.svelte'
 
   export let id
   export let name
@@ -10,28 +7,7 @@
 
   let showEditBoard = false
 
-  const inputFocus = el => el.focus()
-
   const toggleEditBoard = () => showEditBoard = !showEditBoard
-
-  async function updateBoard () {
-    if (!name) return
-
-    await BoardsDB.update(id, name)
-
-    toggleEditBoard()
-
-    // update url
-    $router.push(generateLink(name, id))
-    location.reload()
-  }
-
-  async function deleteBoard () {
-    await BoardsDB.remove(id)
-
-    $router.push('/')
-    location.reload()
-  }
 </script>
 
 <style>
@@ -82,70 +58,6 @@
     padding: 10px;
   }
 
-  /* board edit */
-
-  .board-edit {
-    background-color: var(--white);
-    box-shadow: -3px 0 5px rgba(0, 0, 0, 0.05);
-    height: calc(100vh - 50px);
-    padding: 20px;
-    position: fixed;
-    right: 0;
-    top: 50px;
-    width: 290px;
-    z-index: 997;
-  }
-
-  /* .board edit header */
-
-  .board-edit-header {
-    align-items: center;
-    display: flex;
-    color: var(--text-dark);
-    justify-content: space-between;
-  }
-
-  .board-edit-header > span {
-    font-weight: 500;
-  }
-
-  .board-edit-header > button {
-    color: var(--text-light);
-    padding: 5px;
-  }
-
-  /* board edit form */
-
-  .board-edit-form {
-    display: table;
-    margin-top: 40px;
-  }
-
-  .board-edit-input {
-    margin-top: 10px;
-  }
-
-  .board-btn-update {
-    float: right;
-    margin-top: 20px;
-  }
-
-  /* board edit delete */
-
-  .board-edit-delete {
-    margin-top: 100px;
-    width: 100%;
-  }
-
-  .board-btn-delete {
-    color: var(--text-red);
-    font-size: 14px;
-    font-weight: 500;
-    float: right;
-    margin-right: -5px;
-    padding: 5px;
-  }
-
   @media (min-width: 768px) {
     .board-header {
       margin-top: 40px;
@@ -171,36 +83,12 @@
       font-size: 14px;
       margin-right: 10px;
     }
-
-    /* board edit */
-
-    .board-edit {
-      width: 340px;
-    }
   }
 
   @media (min-width: 1200px) {
     .board-btn-edit {
       margin-right: 30px;
       padding-right: 0;
-    }
-
-    /* board edit */
-
-    .board-edit {
-      height: calc(100vh - 60px);
-      top: 60px;
-    }
-
-    /* board edit delete */
-
-    .board-edit-delete {
-      position: absolute;
-      bottom: 50px;
-    }
-
-    .board-btn-delete {
-      margin-right: 40px;
     }
   }
 </style>
@@ -218,23 +106,5 @@
 </header>
 
 {#if showEditBoard}
-  <aside class="board-edit" transition:fly={{ x: 150 }}>
-    <header class="board-edit-header">
-      <span>Editar Quadro</span>
-
-      <button class="icon-close" on:click={toggleEditBoard}></button>
-    </header>
-
-    <div class="board-edit-form">
-      <label>Nome</label>
-      <textarea type="text" class="board-edit-input" placeholder="Digite o nome do quadro" rows="3"
-        bind:value={name} use:inputFocus></textarea>
-
-      <button class="board-btn-update btn btn-primary" on:click={updateBoard} disabled={!name}>ATUALIZAR</button>
-    </div>
-
-    <div class="board-edit-delete">
-      <button class="board-btn-delete" on:click={deleteBoard}>EXCLUIR QUADRO</button>
-    </div>
-  </aside>
+  <BoardEdit id={id} name={name} toggleEdit={toggleEditBoard} />
 {/if}
