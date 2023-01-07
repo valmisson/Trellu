071ceaae3ed60077a9faa3d54cca5e5ce97ad072
Valmisson Grizorte <missongrizorte@gmail.com>
2023-01-07 20:50:36 -0300

    bump deps

--- rollup.config.js
+++ rollup.config.js
@@ -30,9 +30,7 @@ export default {
 
         // we'll extract any component CSS out into
         // a separate file — better for performance
-        css: css => {
-          css.write('public/bundle.css')
-        }
+        css: true
       },
 
       emitCss: false,
